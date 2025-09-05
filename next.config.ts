f3933a132304e5e3e510aaa77b8d3a91febf1b71
AlexKpac <47696806+AlexKpac@users.noreply.github.com>
2025-09-06 00:36:38 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Говорим Next.js, что нужно экспортировать в статику
+  images: {
+    unoptimized: true, // Отключаем серверную оптимизацию картинок, т.к. сервера не будет
+  },
 };
 
 export default nextConfig;
